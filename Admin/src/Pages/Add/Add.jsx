968adf20aa3483d7fb583e936b47748c6066b41b
saurav kumar <sauravsuman8316@gmail.com>
2025-03-30 21:58:05 +0530

    Updated assets.js while importig

--- Admin/src/Pages/Add/Add.jsx
+++ Admin/src/Pages/Add/Add.jsx
@@ -1,6 +1,6 @@
 import React, {  useState } from 'react'
 import './Add.css'
-import { assets } from '../../assets/assets'
+import { assets } from '../../assets/assets.js'
 import axios from 'axios'
 import { toast } from 'react-toastify'
 
